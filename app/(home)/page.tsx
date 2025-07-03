dcc25cb7fa154de2a01c0de69a42957a49cb81fd
TerrniT <terrnitllc.work@gmail.com>
2025-07-03 21:12:21 +0500

    feat(main): composition of main pages

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -5,14 +5,14 @@ export default function HomePage() {
     <main className="flex flex-1 flex-col justify-center text-center">
       <h1 className="mb-4 text-2xl font-bold">Frontend Questions</h1>
       <p className="text-fd-muted-foreground">
-        You can open{' '}
+        Переходите по{' '}
         <Link
           href="/docs"
           className="text-fd-foreground font-semibold underline"
         >
           /docs
         </Link>{' '}
-        and see the documentation.
+        чтоб посмотреть вопросы
       </p>
     </main>
   );
